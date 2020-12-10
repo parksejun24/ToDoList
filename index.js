9d697e75b643b42fecb1140e593e6a012e28f112
parksejun24 <sunnystpwns@gmail.com>
2020-12-10 14:12:09 +0900

    first js programming

---
+++ index.js
@@ -0,0 +1,41 @@
+var countId = 0;
+
+function createInput() {
+    var checkbox = document.createElement('input');
+    checkbox.id = countId + "_" + "checkBox";
+    checkbox.className = "checkBox"
+    checkbox.type = "checkbox";
+    checkbox.name = "checkBoxList"
+    checkbox.onclick = function () {
+        if (checkbox.checked) {
+            num = String(checkbox.id).substring(0, 1);
+            document.getElementById(num + "_" + "text").style.textDecoration = "line-through";
+        } else {
+            num = String(checkbox.id).substring(0, 1);
+            document.getElementById(num + "_" + "text").style.textDecoration = "none";
+        }
+    }
+    return checkbox;
+}
+
+function createText() {
+    var text = document.createElement('input');
+    text.id = countId + "_" + "text";
+    text.className = "textBox";
+    text.type = "text";
+    text.name = "textList"
+    return text;
+}
+
+function clickButton() {
+    var where = document.getElementById("list");
+    input = document.createElement("br");
+    where.appendChild(input);
+
+    input = createInput();
+    where.appendChild(input);
+
+    input = createText();
+    where.appendChild(input);
+    countId++;
+}
